fdc17b32e9ca65f36771c4dc4bdc55f649584b11
oussama007 <ohamdi776@gmail.com>
2024-07-03 18:10:43 +0100

    Configure React Router for routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
+import CarRent from "./pages/CarRent/CarRent";
+import CarRentDetails from "./pages/CarRentDetails/CarRentDetails";
 
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <Navbar showLogo={true} showSearchBar={true} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/carRent" element={<CarRent />} />
+        <Route path="/carRent/:carId" element={<CarRentDetails />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
